6235381732f92f451ec4ae627a28df604ab27196
dokuroc <dokuroc@gmail.com>
2019-08-23 03:06:06 -0700

    fixed format.cpp implementation and tested it.\

--- src/format.cpp
+++ src/format.cpp
@@ -1,22 +1,19 @@
 #include <string>
-
+#include <iostream>
 #include "format.h"
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 std::string Format::ElapsedTime(long timeInSeconds) {
-    const int Minute = 60;
-    const int Second = 1;
-    const int Hour = 60 * Second * Minute;
-    int hours = timeInSeconds / hours; // hard  set to float then or use int and lose the meaning full numbers? 
-    int minutes = timeInSeconds / 60 - hours * 60;
-    int seconds = timeInSeconds - (hours * 60 * 60) - (minutes * 60);
+    const long int MinuteRepresentedInSeconds = 60;
+    const long int Second = 1;
+    const long int HourRepresentedInSeconds = 60 * Second * MinuteRepresentedInSeconds;
+    long int hours = timeInSeconds / HourRepresentedInSeconds; // hard  set to float then or use int and lose the meaning full numbers? 
+    long int minutes = (timeInSeconds / MinuteRepresentedInSeconds) - (HourRepresentedInSeconds / MinuteRepresentedInSeconds);
+    long int seconds = timeInSeconds - (HourRepresentedInSeconds) - (MinuteRepresentedInSeconds * minutes);
     std::string hoursString = std::to_string(hours);
     std::string minutesString = std::to_string(minutes);
     std::string secondsString = std::to_string(seconds);
     std::string finalTimeOutput = hoursString + ":" + minutesString + ":" + secondsString; 
+    std::cout << finalTimeOutput << "\n" << "shmolick\n";
 
     return finalTimeOutput; 
 }
