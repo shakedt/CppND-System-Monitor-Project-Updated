f0c4f54e235f84e874ab163b4a6a04f09af973f4
dokuroc <dokuroc@gmail.com>
2019-09-03 05:41:14 -0700

    add pid to process.h and set up most of the functions inside process.cpp missing overload and cpuutil

--- src/process.cpp
+++ src/process.cpp
@@ -5,28 +5,39 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
 // TODO: Return this process's ID
-int Process::Pid() { return 0; }
+int Process::Pid() { return pid; }
 
 // TODO: Return this process's CPU utilization
+// need to do calculations here consider renamind vector to int function on linux parser
+// and do calcs there seems to make much more sens
 float Process::CpuUtilization() { return 0; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+// Return the command that generated this process
+string Process::Command() { 
+    return LinuxParser::Command(pid);
+}
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Return this process's memory utilization
+string Process::Ram() { 
+    return LinuxParser::Ram(pid);
+}
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+// Return the user (name) that generated this process
+string Process::User() {
+    return LinuxParser::User(pid); 
+}
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+// Return the age of this process (in seconds)
+long int Process::UpTime() { 
+    return LinuxParser::UpTime(pid);
+}
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
