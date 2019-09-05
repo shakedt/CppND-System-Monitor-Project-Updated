a27b3f3d47bcdbccccb2749cf9b0f9676cc855ae
dokuroc <dokuroc@gmail.com>
2019-09-05 08:42:06 -0700

    add total proccess to system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -37,7 +37,9 @@ int System::RunningProcesses() {
 }
 
 // TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+int System::TotalProcesses() {
+  return LinuxParser::TotalProcesses();
+}
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return 0; }
