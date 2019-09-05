dff85133a450c30c585c051c849a340b89069811
dokuroc <dokuroc@gmail.com>
2019-09-05 08:32:08 -0700

    updated operating system function in system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.cpp"
 
 using std::set;
 using std::size_t;
@@ -25,8 +26,10 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Return the operating system name
+std::string System::OperatingSystem() {
+   return  LinuxParser::Kernel();
+}
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
