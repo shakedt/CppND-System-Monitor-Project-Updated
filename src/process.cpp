09a94a199243a4b6616854329ac07be3ec5b2f69
dokuroc <dokuroc@gmail.com>
2019-09-06 02:19:38 -0700

    upload operator oeverloading in process, implementation is faulty need to fix.

--- src/process.cpp
+++ src/process.cpp
@@ -41,6 +41,10 @@ long int Process::UpTime() {
     return LinuxParser::UpTime(pid);
 }
 
-// TODO: Overload the "less than" comparison operator for Process objects
+// TODO: Overload the "less than" comparison operator for Pocess robjects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const {
+    this->Ram() > a.Ram();
+
+    return true;
+}
